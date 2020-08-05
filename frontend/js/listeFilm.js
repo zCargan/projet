b5e972310a0673a364c3bb463d589eabec4671de
zCargan <64102236+zCargan@users.noreply.github.com>
2020-08-05 14:54:35 +0200

    Add files via upload

--- frontend/js/listeFilm.js
+++ frontend/js/listeFilm.js
@@ -1,3 +1,35 @@
+function makeTr(array) {
+    let chaineTxt = "";
+    chaineTxt += "<p>";
+
+
+}
+
+
+
 function test() {
-    alert("ça marche bien");
+    alert("pq");
+    /*let arrayInfo = [];
+    let nomFilm = document.getElementById("nomDuFilm").value;
+    let plateform = document.getElementById("plateformDeVisionnage").value;
+    let nombreEtoiles = document.getElementById("nbrEtoiles").value;
+    let nombreEpisodes = document.getElementById("nbrEpisodes").value;
+    let origine = document.getElementById("Origine").value;
+    let comm = document.getElementById("commentaire").value;
+    let valeur;
+    if (document.getElementById('film').checked) {
+        valeur = document.getElementById('film').value;
+    }
+    else if (document.getElementById('serie').checked) {
+        valeur = document.getElementById('serie').value;
+    }
+
+    arrayInfo.push(nomFilm);
+    arrayInfo.push(plateform);
+    arrayInfo.push(nombreEtoiles);
+    arrayInfo.push(nombreEpisodes);
+    arrayInfo.push(origine);
+    arrayInfo.push(comm);*/
+    //arrayInfo.push(valeur);
+    //console.log(arrayInfo);
 }
