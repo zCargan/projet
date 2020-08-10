3342ca9a741fefc0bfa6aa31d28326708628d1f2
zCargan <64102236+zCargan@users.noreply.github.com>
2020-08-10 12:49:29 +0200

    Add files via upload

--- frontend/js/listeFilm.js
+++ frontend/js/listeFilm.js
@@ -1,35 +1,41 @@
+let chaineTxt = "";
+
 function makeTr(array) {
     let chaineTxt = "";
-    chaineTxt += "<p>";
-
-
+    chaineTxt += "<tr>";
+    for(let i = 0; i < array.length -1; i ++) {
+        chaineTxt += "<td>";
+        chaineTxt += array[i];
+        chaineTxt += "</td>";
+    }
+    chaineTxt += "</tr>";
+    return chaineTxt;
 }
 
 
 
 function test() {
-    alert("pq");
-    /*let arrayInfo = [];
+    //alert("pq");
+    let arrayInfo = [];
     let nomFilm = document.getElementById("nomDuFilm").value;
     let plateform = document.getElementById("plateformDeVisionnage").value;
     let nombreEtoiles = document.getElementById("nbrEtoiles").value;
     let nombreEpisodes = document.getElementById("nbrEpisodes").value;
     let origine = document.getElementById("Origine").value;
     let comm = document.getElementById("commentaire").value;
-    let valeur;
-    if (document.getElementById('film').checked) {
-        valeur = document.getElementById('film').value;
-    }
-    else if (document.getElementById('serie').checked) {
-        valeur = document.getElementById('serie').value;
-    }
-
+    let fous = document.getElementById("fous").value;
     arrayInfo.push(nomFilm);
     arrayInfo.push(plateform);
     arrayInfo.push(nombreEtoiles);
     arrayInfo.push(nombreEpisodes);
     arrayInfo.push(origine);
-    arrayInfo.push(comm);*/
-    //arrayInfo.push(valeur);
-    //console.log(arrayInfo);
+    arrayInfo.push(comm);
+    arrayInfo.push(fous);
+    makeTr(arrayInfo);
+    if(fous == "serie") {
+        document.getElementById("tableSerie").innerHTML += chaineTxt;    
+    } else {
+        document.getElementById("tableFilm").innerHTML += chaineTxt;
+
+    }
 }
